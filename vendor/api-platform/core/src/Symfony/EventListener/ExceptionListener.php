911da232eb20d5d241f31e512db012e561a82f72
mohammed ahadjji <mohammed.ahadjji@ump.ac.ma>
2024-01-06 04:04:04 +0100

    update

---
+++ vendor/api-platform/core/src/Symfony/EventListener/ExceptionListener.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Symfony\EventListener;
+
+use ApiPlatform\Metadata\Error;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\EventListener\ErrorListener;
+
+/**
+ * Handles requests errors.
+ *
+ * @author Samuel ROZE <samuel.roze@gmail.com>
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ */
+final class ExceptionListener
+{
+    public function __construct(private readonly ErrorListener $errorListener)
+    {
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $request = $event->getRequest();
+        // Don't loop on errors leave it to Symfony as we could not handle this properly
+        if (($operation = $request->attributes->get('_api_operation')) && $operation instanceof Error) {
+            return;
+        }
+
+        $this->errorListener->onKernelException($event);
+    }
+}
